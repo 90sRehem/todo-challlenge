e70c817f114d5e9d8d6792ffa1cf17c78138181a
Jonathan Rehem <jonathan.de.oliveira@live.com>
2023-02-19 18:52:08 -0300

    feat: add edit func to todo

--- apps/web/src/components/List/List.tsx
+++ apps/web/src/components/List/List.tsx
@@ -2,12 +2,10 @@ import { ClipboardText, Trash } from 'phosphor-react'
 import * as Styles from './List.styles'
 import { Checkbox } from '../Checkbox'
 import { IconButton } from '../IconButton'
-
-interface Todo {
-    id: string
-    title: string
-    completed: boolean
-}
+import { Todo } from '../../types'
+import { Input } from '../Input'
+import { useRef, useState } from 'react'
+import { useEventListener, useOnClickOutside } from '../../hooks'
 
 interface ListProps {
     todos: Todo[]
@@ -16,6 +14,36 @@ interface ListProps {
 }
 
 export function List({ todos, removeTodo, markAsDone }: ListProps) {
+    const [editing, setEditing] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const cardRef = useRef<HTMLDivElement>(null);
+    const liRef = useRef<HTMLLIElement>(null);
+    const completedTodos = todos.filter(todo => todo.done)
+
+    const handleEdit = (id: string) => {
+        setEditing(state => {
+            if (state === id) {
+                return '';
+            }
+
+            return id;
+        })
+    }
+
+    const handleClickOutside = () => handleEdit(editing);
+    const handleKeyPress = (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            handleEdit(editing);
+        }
+        if (e.key === 'Escape') {
+            console.log("🚀 ~ file: List.tsx:42 ~ handleKeyPress ~ e.key === 'Escape'", e.key === 'Escape')
+            handleEdit(editing);
+        }
+    }
+    // useEventListener('keyup', handleKeyPress, inputRef);
+    // useOnClickOutside(liRef, handleClickOutside)
+
     return (
         <Styles.Container>
             <Styles.Summary>
@@ -23,16 +51,40 @@ export function List({ todos, removeTodo, markAsDone }: ListProps) {
                     Tarefas criadas <Styles.Badge>{todos.length}</Styles.Badge>
                 </strong>
                 <strong>
-                    Concluídas <Styles.Badge>0 de {todos.length}</Styles.Badge>
+                    Concluídas <Styles.Badge>{completedTodos.length} de {todos.length}</Styles.Badge>
                 </strong>
             </Styles.Summary>
             <Styles.List>
                 {todos.length > 0 ? (
                     todos.map((item, index) => (
-                        <Styles.Item key={index}>
-                            <Checkbox name='checkbox' checked={item.completed} onCheckedChange={() => markAsDone(item.id)} />
-                            <label htmlFor="checkbox">{item.title}</label>
-                            <IconButton type='button' onClick={() => removeTodo(item.id)} />
+                        <Styles.Item key={index} ref={liRef}>
+                            <Styles.Card >
+                                <Checkbox
+                                    name='checkbox'
+                                    checked={item.done}
+                                    onCheckedChange={() => markAsDone(item.id)}
+                                />
+                                <label
+                                    htmlFor="checkbox"
+                                    data-checked={item.done}
+                                    data-visible={editing === item.id}
+                                    // onDoubleClick={() => {
+                                    //     handleEdit(item.id);
+                                    // }}
+                                >
+                                    {item.title}
+                                </label>
+                                {/* <Input
+
+                                    ref={input => input && input.focus()}
+                                    data-visible={editing === item.id}
+                                    name={'edit'}
+                                    placeholder={'Digite o novo título da tarefa'}
+                                // value={item.title}
+                                defaultValue={item.title}
+                                /> */}
+                                <IconButton type='button' onClick={() => removeTodo(item.id)} />
+                            </Styles.Card>
                         </Styles.Item>
                     ))
                 ) : (
